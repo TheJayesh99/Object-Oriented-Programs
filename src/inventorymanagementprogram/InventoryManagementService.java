0a717e7c2cd94d5344c5a5b38dcd3ce04b08a9a9
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-05 20:45:00 +0530

    Added : Inventory management program

---
+++ src/inventorymanagementprogram/InventoryManagementService.java
@@ -0,0 +1,20 @@
+package inventorymanagementprogram;
+
+public interface InventoryManagementService 
+{
+
+	//method to add item in inventory
+	public  void addItem(ItemsDetails item);
+	//method to get all details in inventory
+	public void getDetails();
+	//method to get details of particular item
+	public void getDetailsByName(String itemName);
+	//method to calculate value of each item
+	public void calculateValueOfEachItem();
+	//method to calculate value of all the items
+	public void getTotalValue();
+	//method to update value of item in inventory
+	public void updateValue(ItemsDetails updateItem);
+	//method to remove item in inventory
+	public void deleteValue(String  deleteItem);
+}
